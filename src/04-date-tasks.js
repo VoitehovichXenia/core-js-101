30dac02b70052e732725864d222a0b9ec13747cd
VoitehovichXenia <voitehovichkg@gmail.com>
2022-07-24 00:48:51 +0300

    feat: add solutions to date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,11 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const passedDate = new Date(date);
+  const year = passedDate.getFullYear();
+  const isLeap = (new Date(year, 1, 29)).getMonth() === 1;
+  return isLeap;
 }
 
 
@@ -73,8 +76,26 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  let difference = Math.abs(startDate.getTime() - endDate.getTime());
+  let time = [];
+
+  for (let i = 0; i < 4; i += 1) {
+    const divisor = i === 0 ? 1000 : 60;
+    time.push(difference % divisor);
+    difference = (difference - (difference % divisor)) / divisor;
+  }
+  time = time.reverse().map((item, i, arr) => {
+    if (i === arr.length - 1 && item < 10) return `00${item}`;
+    if ((i === arr.length - 1 && item < 100)) return `0${item}`;
+    if (item < 10) return `0${item}`;
+    return item;
+  }).reduce((prev, curr, i, arr) => {
+    if (i === arr.length - 1) return `${prev}.${curr}`;
+    if (i === 0) return prev + curr;
+    return `${prev}:${curr}`;
+  }, '');
+  return time;
 }
 
 
@@ -94,10 +115,25 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const HOUR_HAND_SPEED = 360 / (12 * 60);
+  const MINUTE_HAND_SPEED = 360 / 60;
+  const minutes = Math.floor(date.getTime() / 1000) / 60;
+  let hourHandAngle = (minutes * HOUR_HAND_SPEED) % 360;
+  let minuteHandAngle = (minutes * MINUTE_HAND_SPEED) % 360;
+  let hours = date.getUTCHours();
+  hours = hours > 12 ? hours - 12 : hours;
+  if (minuteHandAngle === 0) {
+    minuteHandAngle = hours < 6 ? 0 : 360;
+  }
+  if (hourHandAngle === 0) {
+    hourHandAngle = hours < 6 ? 0 : 360;
+  }
+  return (
+    (Math.max(hourHandAngle, minuteHandAngle) - Math.min(hourHandAngle, minuteHandAngle))
+    * Math.PI
+  ) / 180;
 }
-
 
 module.exports = {
   parseDataFromRfc2822,
